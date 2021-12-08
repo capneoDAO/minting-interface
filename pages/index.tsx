43b1e252c694a56fc12c0db0b3689ad1cf22fd3a
DAC-info <luca.eich@thedac.info>
2021-12-09 03:42:04 +0900

    finalize design, remove carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from "next/image"
 import { BigNumber, ethers } from 'ethers';
 import { AiFillPlusCircle } from "react-icons/ai"
 import { AiFillMinusCircle } from "react-icons/ai"
@@ -30,7 +31,18 @@ const Home: NextPage = () => {
     const [hash, setHash] = useState("")
     const [NFTs, setNFTs] = useState({})
     const [showNFTs, setShowNFTs] = useState(false)
+    const [number, setNumber] = useState(3)
 
+    const increaseNumber = () => {
+        if (number < 11) {
+            setNumber(number + 1)
+        } else if (number === 11) {
+            setNumber(3)
+        }
+    }
+    useEffect(() => {
+        setTimeout(increaseNumber, 3000)
+    }, [number])
 
     const calcPrice = () => {
         if (process.env.NEXT_PUBLIC_PRICE) {
@@ -104,7 +116,7 @@ const Home: NextPage = () => {
                 <TransactionModal onDismiss={() => { setTransactionModal(false); !transactionLoading && window.location.reload() }} loading={transactionLoading} success={success} hash={hash} chainId={chainId} />
             )}
 
-            <main className="flex flex-col items-start w-screen overflow-hidden">
+            <main className="flex flex-col w-screen">
                 <div className="w-full flex justify-between items-center p-5 lg:p-10" >
                     <a href="https://www.metagamehub.io" target="_blank" className="transform hover:scale-110 transition-all duration-500 ease-in-out">
                         <img src="/images/mgh_logo.png" className={`h-12 lg:h-18`} />
@@ -113,102 +125,78 @@ const Home: NextPage = () => {
                 </div>
 
 
-                <div className="flex flex-col w-full">
+                <div className="flex flex-col items-center w-full">
                     <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-pink-500 text-center mt-10 p-5">Meta NFT Portraits</h1>
 
-                    <div className="flex w-full min-w-max h-screen items-center trick">
-                        <div className="flex slideshow w-full h-3/4">
-                            <img src="/images/nfts/0.gif" className="nft-image" />
-                            <img src="/images/nfts/1.gif" className="nft-image" />
-                            <img src="/images/nfts/2.gif" className="nft-image" />
-                            <img src="/images/nfts/3.jpg" className="nft-image" />
-                            <img src="/images/nfts/4.jpg" className="nft-image" />
-                            <img src="/images/nfts/5.jpg" className="nft-image" />
-                            <img src="/images/nfts/6.jpg" className="nft-image" />
-                            <img src="/images/nfts/7.jpg" className="nft-image" />
-                            {/* <img src="/images/nfts/8.jpg" className="nft-image" />
-                            <img src="/images/nfts/9.jpg" className="nft-image" /> */}
-                            {/* <img src="/images/nfts/11.jpg" className="nft-image" />
-                            <img src="/images/nfts/12.jpg" className="nft-image" /> */}
-                        </div>
+                    <div className="flex flex-col max-w-lg lg:max-w-full lg:flex-row select-none shadow-color bg-gray-900 rounded-xl p-5 xl:p-8 mx-5 sm:mx-20 lg:mx-0 items-stretch justify-evenly space-y-10 lg:space-y-0 space-x-0 lg:space-x-10 mt-20">
 
-                        <div className="flex slideshow w-full h-3/4">
-                            <img src="/images/nfts/0.gif" className="nft-image" />
-                            <img src="/images/nfts/1.gif" className="nft-image" />
-                            <img src="/images/nfts/2.gif" className="nft-image" />
-                            <img src="/images/nfts/3.jpg" className="nft-image" />
-                            <img src="/images/nfts/4.jpg" className="nft-image" />
-                            <img src="/images/nfts/5.jpg" className="nft-image" />
-                            <img src="/images/nfts/6.jpg" className="nft-image" />
-                            <img src="/images/nfts/7.jpg" className="nft-image" />
-                            {/* <img src="/images/nfts/8.jpg" className="nft-image" />
-                            <img src="/images/nfts/9.jpg" className="nft-image" /> */}
-                            {/* <img src="/images/nfts/11.jpg" className="nft-image" />
-                            <img src="/images/nfts/12.jpg" className="nft-image" /> */}
-                        </div>
+                        <img src={`/images/nfts/${number}.jpg`} className="rounded-lg h-auto lg:h-96 xl:h-full" />
+                        
 
-                    </div>
+                        <div className="flex flex-col justify-end p-1 px-0 lg:px-10 xl:px-20">
 
-                    <div className="flex flex-col self-center select-none shadow-color bg-gray-900 rounded-xl p-5 transform scale-70 xs:scale-75 sm:scale-100">
-                        <div className="flex items-stretch space-x-8 pb-4">
-                            <div className="flex flex-col items-center pt-3">
-                                <p className="text-gray-400 font-medium self-start pl-2">Quantity</p>
-                                <div className="flex items-center space-x-2 justify-center">
-                                    <AiFillPlusCircle onClick={increase} className="text-5xl text-gray-700 hover:text-gray-600 transition-all ease-in-out duration-300 cursor-pointer" />
-                                    <p className="text-pink-600 text-6xl font-medium w-18 text-center pt-2">{quantity}</p>
-                                    <AiFillMinusCircle onClick={decrease} className="text-5xl text-gray-700 hover:text-gray-600 transition-all ease-in-out duration-300 cursor-pointer" />
+                            <div className="flex items-center justify-between space-x-5 sm:space-x-10 mb-5 px-3">
+                                <p className="text-gray-400 font-medium text-2xl sm:text-3xl pt-1">Quantity: </p>
+                                <div className="flex items-center space-x-0 sm:space-x-1 justify-center">
+                                    <AiFillPlusCircle onClick={increase} className="text-4xl sm:text-5xl text-gray-700 hover:text-gray-600 transition-all ease-in-out duration-300 cursor-pointer" />
+                                    <p className="text-pink-600 text-5xl sm:text-6xl font-medium w-18 text-center pt-2">{quantity}</p>
+                                    <AiFillMinusCircle onClick={decrease} className="text-4xl sm:text-5xl text-gray-700 hover:text-gray-600 transition-all ease-in-out duration-300 cursor-pointer" />
                                 </div>
                             </div>
 
-                            <div className="flex flex-col items-center bg-gray-800 rounded-lg p-3">
-                                <p className="text-gray-400 font-medium self-start pl-2 pb-3">Price</p>
+                            <div className="flex items-center justify-between space-x-10 bg-gray-800 rounded-xl p-3 py-4 sm:py-5 mb-10">
+                                <p className="text-gray-400 font-medium text-2xl sm:text-3xl pt-1">Price: </p>
                                 <div className="flex items-end">
-                                    <p className="text-gray-300 text-4xl font-medium w-24 text-center">{calcPrice()}</p>
-                                    <p className="text-gray-400 text-2xl">ETH</p>
+                                    <p className="text-gray-300 text-3xl sm:text-4xl font-medium w-24 text-center pt-1">{calcPrice()}</p>
+                                    <p className="text-gray-400 text-2xl sm:text-3xl pt-1">ETH</p>
                                 </div>
                             </div>
-                        </div>
 
-                        {!web3Provider && (
-                            <button onClick={() => setOpenModal(true)} className="z-30 mt-4 relative flex justify-center items-center transition ease-in-out duration-500 rounded-xl w-full py-3 sm:py-4 group self-center">
-                                <div className="h-full w-full absolute bg-gradient-to-br transition-all ease-in-out duration-300 from-pink-600 to-blue-500 rounded-xl opacity-60 group-hover:opacity-80" />
-                                <span className="pt-1 z-10 text-gray-200 font-medium text-lg sm:text-xl">Connect Wallet</span>
-                            </button>
-                        )}
-                        {web3Provider && chainId === Chains.ETHEREUM_MAINNET.chainId && (<>
-                            <button disabled={loading} onClick={mint} className="disabled:opacity-50 disabled:cursor-default mt-4 relative flex justify-center items-center shadow-black rounded-xl w-full py-3 sm:py-4 group self-center">
-                                <div className="h-full w-full absolute bg-pink-600 transition-all ease-in-out duration-300 rounded-xl opacity-100 group-disabled:group-hover:opacity-100 group-hover:opacity-80" />
-                                <span className="pt-1 z-10 text-grey-darkest font-medium text-lg sm:text-xl">Mint NFTs</span>
-                            </button>
-                            <button disabled={loading} onClick={getData} className="disabled:opacity-50 disabled:cursor-default mt-4 relative flex justify-center items-center transition ease-in-out duration-300 rounded-xl w-full py-3 sm:py-4 group self-center">
-                                <div className="h-full w-full absolute border border-pink-600 transition-all ease-in-out duration-300 rounded-xl opacity-100 group-disabled:group-hover:opacity-100 group-hover:opacity-80" />
-                                <span className="pt-1 z-10 text-pink-600 font-medium text-lg sm:text-xl transition-all ease-in-out duration-300 text-opacity-100 group-disabled:group-hover:text-opacity-100 group-hover:text-opacity-80">Show my NFTs</span>
-                            </button>
-                        </>
-                        )}
-                        {web3Provider && chainId !== Chains.ETHEREUM_MAINNET.chainId && (
-                            <button onClick={() => { changeChain(web3Provider.provider, Chains.ETHEREUM_MAINNET.chainId) }} className="z-30 mt-4 relative flex justify-center items-center transition ease-in-out duration-500 rounded-xl w-full py-3 sm:py-4 group self-center">
-                                <div className="h-full w-full absolute bg-gradient-to-br transition-all ease-in-out duration-300 from-pink-600 to-blue-500 rounded-xl opacity-60 group-hover:opacity-80" />
-                                <span className="pt-1 z-10 text-gray-200 font-medium text-lg sm:text-xl">Switch to Ethereum</span>
-                            </button>
-                        )}
+                            {!web3Provider && (
+                                <button onClick={() => setOpenModal(true)} className="z-30 mt-4 relative flex justify-center items-center transition ease-in-out duration-500 rounded-xl w-full py-3 sm:py-4 group self-center">
+                                    <div className="h-full w-full absolute bg-gradient-to-br transition-all ease-in-out duration-300 from-pink-600 to-blue-500 rounded-xl opacity-60 group-hover:opacity-80" />
+                                    <span className="pt-1 z-10 text-gray-200 font-medium text-lg sm:text-xl">Connect Wallet</span>
+                                </button>
+                            )}
+                            {web3Provider && chainId === Chains.ETHEREUM_MAINNET.chainId && (<>
+                                <button disabled={loading} onClick={mint} className="disabled:opacity-50 disabled:cursor-default mt-4 relative flex justify-center items-center shadow-black rounded-xl w-full py-3 sm:py-4 group self-center">
+                                    <div className="h-full w-full absolute bg-pink-600 transition-all ease-in-out duration-300 rounded-xl opacity-100 group-disabled:group-hover:opacity-100 group-hover:opacity-80" />
+                                    <span className="pt-1 z-10 text-grey-darkest font-medium text-lg sm:text-xl">Mint NFTs</span>
+                                </button>
+                                <button disabled={loading} onClick={getData} className="disabled:opacity-50 disabled:cursor-default mt-4 relative flex justify-center items-center transition ease-in-out duration-300 rounded-xl w-full py-3 sm:py-4 group self-center">
+                                    <div className="h-full w-full absolute border border-pink-600 transition-all ease-in-out duration-300 rounded-xl opacity-100 group-disabled:group-hover:opacity-100 group-hover:opacity-80" />
+                                    <span className="pt-1 z-10 text-pink-600 font-medium text-lg sm:text-xl transition-all ease-in-out duration-300 text-opacity-100 group-disabled:group-hover:text-opacity-100 group-hover:text-opacity-80">Show my NFTs</span>
+                                </button>
+                            </>
+                            )}
+                            {web3Provider && chainId !== Chains.ETHEREUM_MAINNET.chainId && (
+                                <button onClick={() => { changeChain(web3Provider.provider, Chains.ETHEREUM_MAINNET.chainId) }} className="z-30 mt-4 relative flex justify-center items-center transition ease-in-out duration-500 rounded-xl w-full py-3 sm:py-4 group self-center">
+                                    <div className="h-full w-full absolute bg-gradient-to-br transition-all ease-in-out duration-300 from-pink-600 to-blue-500 rounded-xl opacity-60 group-hover:opacity-80" />
+                                    <span className="pt-1 z-10 text-gray-200 font-medium text-lg sm:text-xl">Switch to Ethereum</span>
+                                </button>
+                            )}
+                        </div>
                     </div>
+
                 </div>
 
+
+
+
                 {loading && (
-                    <div className="w-full h-full self-center flex items-center justify-center mt-20">
+                    <div className="w-full h-full self-center flex items-center justify-center mt-10">
                         <img src="/images/mgh_logo.png" className={` h-24 w-24 animate-rotate`} />
                     </div>
                 )}
 
                 {showNFTs && address && (
                     Object.entries(NFTs).length === 0 ? (
-                        <p className="font-medium text-lg text-gray-300 my-10 text-center self-center">You have no Meta NFT Portraits in your Wallet</p>
+                        <p className="font-medium text-lg text-gray-300 my-10 text-center self-center px-5">You have no Meta NFT Portraits in your Wallet</p>
                     ) : (
-                        <div className="flex flex-wrap w-screen h-auto py-10 my-10">
+                        <div className="flex items-center justify-center flex-wrap w-screen h-auto py-10 my-10">
                             {Object.entries(NFTs).map(([id, imageLink]: any) => {
                                 return (
-                                    <div key={id} className="relative w-2/12">
+                                    <div key={id} className="relative w-4/12 lg:w-2/12">
                                         <div className="absolute top-0 left-0 w-full h-full backdrop-filter backdrop-blur-xl bg-black bg-opacity-30 flex flex-col items-center justify-center p-2 opacity-0 hover:opacity-100 transition ease-in-out duration-300 cursor-pointer">
                                             <a href={`https://opensea.io/assets/0x657c7e3b1d32bc4e757a2648a004d2f50d83d6a0/${id}`} target="_blank" className="text-gray-200 text-lg font-medium text-center">View on Opensea</a>
                                         </div>
@@ -221,9 +209,9 @@ const Home: NextPage = () => {
                 )}
 
 
-                <div className="relative w-full h-60 xs:h-72 lg:h-96 overflow-hidden">
+                <div className="relative w-full h-60 xs:h-72 lg:h-96 overflow-hidden mt-10">
                     <img src="/images/mgh_logo.png" className="w-3/4 absolute top-10 -left-1/4 blur sm:blur-md lg:blur-xl" />
-                    <div className="flex flex-row items-center w-full justify-center sm:justify-end flex-wrap space-x-3 absolute bottom-0 right-0 p-10">
+                    <div className="flex flex-row items-center w-full justify-center sm:justify-end flex-wrap space-x-3 absolute bottom-0 right-0 p-8 xs:p-10">
                         <a href="https://metagamehub.medium.com" className="cursor-pointer" target="_blank" >
                             <FaMedium className="social-media-icon" />
                         </a>
