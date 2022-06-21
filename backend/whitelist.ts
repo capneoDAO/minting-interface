2948ebf2d2750639e6b43c8994e7c9b8efa3d1bc
DAC-PhilThomsen <phil.thomsen@thedac.info>
2022-06-21 19:29:52 +0200

    always use checksum address

--- backend/whitelist.ts
+++ backend/whitelist.ts
@@ -1,15 +1,15 @@
 import { whitelists } from "./whitelistArray";
 import { getProof } from "../lib/utilities";
-import { isAddress, } from "ethers/lib/utils";
+import { isAddress, getAddress } from "ethers/lib/utils";
 
 export const isAddressWhitelisted = (address: any, activePhase: number): boolean => {
     if(! isAddress(address)) return false;
 
-    return whitelists[activePhase].includes(address.toLowerCase());
+    return whitelists[activePhase].includes(getAddress(address));
 }
 
 export const getProofForAddress = (address: any, activePhase: number): Array<string> => {
-    console.log(activePhase)
+    console.log({activePhase}, whitelists[activePhase], "proof: ", getProof(address, whitelists[activePhase]))
     if(! isAddress(address)) return [];
 
     return getProof(address, whitelists[activePhase]);
--- backend/whitelistArray.ts
+++ backend/whitelistArray.ts
@@ -1,8 +1,8 @@
 export const whitelists = [
     [],
     [
-        '0xfca6b83afbbb0d66a13a06ec31fa8e27e5188ca7', 
-        '0x382511ab458a8e55be45f81bdb78c0b25e2148f2'
+        '0x382511Ab458A8e55be45F81BDb78c0b25E2148F2', 
+        '0xFca6b83AfBBB0d66A13a06Ec31fA8e27E5188ca8'
     ],
     [
 
